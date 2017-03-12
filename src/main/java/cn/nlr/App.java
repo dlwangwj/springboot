ad92d691aad646f2e563a2477a401e41a8d845cf
dlwangwj <dlwangwj@cn.ibm.com>
2017-03-12 13:51:19 +0800

    init commit

--- src/main/java/cn/nlr/App.java
+++ src/main/java/cn/nlr/App.java
@@ -1,13 +1,25 @@
 package cn.nlr;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App.class);
+	}
     public static void main( String[] args )
     {
+    	SpringApplication.run(App.class, args);
         System.out.println( "Hello World!" );
     }
 }
